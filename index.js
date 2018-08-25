df87b7cb3707c710139888f5ad1580944053f4b3
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-25 14:31:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function deepestChild() {
+  let nodes = document.querySelectorAll('#grand-node div')
+  return nodes[nodes.length- 1];
+}
+
+function increaseRankBy(n) {
+  let list = document.querySelectorAll('.ranked-list')
+  list.forEach(function (el) {
+    el.innerHTML = (parseInt(el.innerHTML)+n);
+  })
+}
